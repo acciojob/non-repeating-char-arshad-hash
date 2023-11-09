4302c5a6adeee894ca6c18fc92856839b9091c60
arshad-hash <142026560+arshad-hash@users.noreply.github.com>
2023-11-09 19:12:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+	//for that i will first 
+	for (let i=0; i<str.length ; i++)
+		{
+			let repeated=false;
+			for(let j=0; j<str.length ; j++)
+				{
+					if(i!=j && str[i]===str[j])
+					{ 
+					     repeated=true;
+						break;
+					}
+                }
+			if(!repeated)
+			{
+				return str[i];
+			}
+        } return null;
 }
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+//const input = prompt("Enter a string");
+//alert(firstNonRepeatedChar(input)); 
